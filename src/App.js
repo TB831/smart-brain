f42b26485a47ab2c32150dccd03c22d3c382752a
TB831 <druu831@gmail.com>
2018-06-13 13:14:08 -0700

    added set state to empty. onInputChange change state

--- src/App.js
+++ src/App.js
@@ -20,6 +20,17 @@ const particlesOptions = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="App">
@@ -29,7 +40,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange}/>
         {/* <FaceRecognition /> */}
       </div>
     );
