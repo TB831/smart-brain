19aface9186617724ed45158e97c42d2230228ad
TB831 <druu831@gmail.com>
2018-06-13 12:49:09 -0700

    added new package particles.js. changed the background to particles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import Rank from './components/Rank/Rank.js';
@@ -6,10 +7,25 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import './App.css';
 import 'tachyons';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800,
+      }
+    }
+  }
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+              params={particlesOptions}
+            />
         <Navigation />
         <Logo />
         <Rank />
@@ -18,6 +34,6 @@ class App extends Component {
       </div>
     );
   }
-}
+};
 
 export default App;
