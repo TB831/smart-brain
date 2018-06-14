1dd935b53262cf1421dc2bb6a3bb97bc4eb9144d
TB831 <druu831@gmail.com>
2018-06-13 18:03:57 -0700

    set states to allow proper sign in, sign out and register

--- src/App.js
+++ src/App.js
@@ -34,7 +34,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false,
     }
   }
 
@@ -70,6 +71,12 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    }
+    else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
     this.setState({route: route});
   }
 
@@ -79,7 +86,7 @@ class App extends Component {
         <Particles className='particles'
               params = {particlesOptions}
             />
-        <Navigation onRouteChange={this.onRouteChange}/>
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
         {this.state.route === 'home'
           ? <div>
               <Logo />
